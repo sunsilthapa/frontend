3285179f24dde5a2588a8db46b7e03d59006c0fa
sunsilthapa <sunsil1281@gmail.com>
2023-11-29 07:17:21 +0545

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () =>{
+    return (
+        <div>
+            <h1>Welcome to Homepage folder</h1>
+        </div>
+    )
+}
+
+export default Homepage;
